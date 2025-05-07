22dee0d7b954849b8c02d9730a17786c5cca2455
Wyyshnya <jefersonhrm1@gmail.com>
2025-05-07 11:31:11 +0300

    added .env.example

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/task_manager")
 SECRET_KEY = os.getenv("SECRET_KEY", "63f4945d921d599f27ae4fdf5bada3f1")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-REFRESH_TOKEN_EXPIRE_DAYS = 7
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
